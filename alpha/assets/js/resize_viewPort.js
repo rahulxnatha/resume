8f43a1fb7856dc970fa0da47f467e60d496e2678
Rahul Natha <67892338+hey-rahul@users.noreply.github.com>
2023-09-03 18:01:16 +0530

    Dynamic layout move main with aside drag

--- alpha/assets/js/resize_viewPort.js
+++ alpha/assets/js/resize_viewPort.js
@@ -30,6 +30,9 @@ document.addEventListener('mousemove', (e) => {
     A_offsetY = A_currentY;
 
     viewport.style.transform = `translate(${A_currentX}px, ${A_currentY}px)`;
+
+   
+      
 });
 
 document.addEventListener('mouseup', () => {
@@ -37,3 +40,37 @@ document.addEventListener('mouseup', () => {
     viewport.style.cursor = 'grab';
 });
 viewport.style.cursor = 'grab';
+
+
+
+
+
+    document.addEventListener('mousemove', function(event) {
+
+        if (A_isDragging) {
+        // Get the cursor's X-coordinate
+        const cursorX = event.clientX;
+      
+        // Get the document's width
+        const documentWidth = document.documentElement.clientWidth;
+      
+        // Calculate the percentage position of the cursor
+        const cursorPercentage = (cursorX / documentWidth) * 100;
+      
+        // Check if the cursor is between 0% and 40% of the left side
+        if (cursorPercentage >= 0 && cursorPercentage <= 40) {
+          // Cursor is within the desired range
+          console.log('Cursor is between 0% and 40% of the left side.');
+          document.getElementById('main').style.float = "right";
+
+
+        } else {
+          // Cursor is outside the desired range
+          console.log('Cursor is not in the specified range.');
+          document.getElementById('main').style.float = "left";
+        }
+
+    }
+      });
+
+    
