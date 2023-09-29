d36bdeb13434901ec3e0b1a0c785b7365f833f32
Rahul Natha <67892338+hey-rahul@users.noreply.github.com>
2023-09-29 16:58:45 +0530

    Search engine optimisation

--- alpha/assets/js/search.js
+++ alpha/assets/js/search.js
@@ -1,19 +1,60 @@
 
-let searchBar = document.getElementById('search_bar');
+var searchBar = document.getElementById('search_bar');
 let searchButton = document.getElementById('search_button_action');
+
+
 
 searchButton.addEventListener('click', function search() {
 
     // Get the text of the search query.
-    let searchQuery = searchBar.value;
-
+    var searchQuery = searchBar.value;
     document.getElementById('searchQuery').innerText = searchQuery;
+
+    function extractKeyword(sentence) {
+        const keywords = {
+            // study: [],
+            education: ['education', 'educational', 'qualifications', 'qualification', 'school', 'study', 'studied', 'studying'],
+            work: ['experience', 'work', 'internship','job','corporate','company','sector','profession','field']
+
+
+
+            // Add more keywords and corresponding mappings as needed
+        };
+
+        // Convert the sentence to lowercase for case-insensitive matching
+        const lowerCaseSentence = sentence.toLowerCase();
+
+        // Iterate through the keywords and check if any of them appear in the sentence
+        for (const keyword in keywords) {
+            if (keywords[keyword].some(key => lowerCaseSentence.includes(key))) {
+                return keyword;
+            }
+        }
+
+        // Return a default value if no keyword is found
+        return 'not found';
+    }
+
+
+    // Split the sentence into an array of words.
+    let words = searchQuery.split(' ');
+
+    // Remove any empty strings from the array.
+    let filteredWords = words.filter(word => word !== '');
+
+    // The filteredWords array now contains all of the words in the sentence.
+    console.log(filteredWords);
+
+
+
 
     // For each article on the page, check if the article contains the search query.
     let articles = document.querySelectorAll('article');
     let article_remove_count = 0;
+
+
     for (let article of articles) {
-        // Check if the article contains the search query in the title, body, or any of the tags.
+
         let articleText = article.textContent;
         let articleTags = article.querySelectorAll('*');
         let articleTagsText = '';
@@ -21,27 +62,26 @@ searchButton.addEventListener('click', function search() {
             articleTagsText += tag.textContent;
         }
 
-        // If the article does not contain the search query, hide the article.
-        if (!articleText.includes(searchQuery) && !articleTagsText.includes(searchQuery)) {
+        searchQuery = extractKeyword(searchQuery);
+
+        if (!articleText.toLowerCase().includes(searchQuery.toLowerCase()) && !articleTagsText.toLowerCase().includes(searchQuery.toLowerCase())) {
             article.style.display = 'none';
             article_remove_count++;
             console.log("If the article does not contain the search query, hide the article.");
 
         } else {
-            // If the article does contain the search query, display the article.
+
             article.style.display = 'block';
             console.log("If the article does contain the search query, display the article.");
         }
     }
-
-
 
     document.getElementById('cubeSection').style.display = 'none';
     document.getElementById('banner').style.display = 'none';
 
     document.getElementById('results_count').innerText = document.querySelector('body').getElementsByTagName('article').length - article_remove_count;
 
-    if (document.querySelector('body').getElementsByTagName('article').length - article_remove_count < 1) {
+    if (document.querySelector('body').getElementsByTagName('article').length - article_remove_count < 1 || searchQuery == "") {
         document.getElementById('showing_search_results').style.display = "block";
         document.getElementById('showing_search_results').innerText = "Can you please check the search query for typos or use other keywords? I am unable to get any results with this query. I am still under development, but I hope to be able to answer this query in the future.";
         document.getElementById('whether_showing_search_results').innerText = "Sorry, can't show";
@@ -55,23 +95,15 @@ searchButton.addEventListener('click', function search() {
 
     for (let i = 4; i < spanElements.length; i++) {
         spanElements[i].style.display = 'none';
-        console.log(i);
     }
 
     document.getElementById('search_result_section').style.display = 'block';
 });
 
-
-// let searchBar = document.getElementById('search_bar');
 searchBar.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        // Submit the search form.
-        // document.getElementById('search_form').submit();
         document.getElementById('search_button_action').click();
     }
 });
 
 document.getElementById('search_result_section').style.display = 'none';
-
-
-// Array.from(document.querySelectorAll('main section span')).style.display = 'none';
