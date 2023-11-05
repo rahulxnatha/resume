a654da64ca7f052a5f97b44f687c36004a9d3ad7
Rahul Natha <67892338+hey-rahul@users.noreply.github.com>
2023-11-05 18:59:56 +0530

    Preview box update for mobile

--- assets/js/de-resize_viewPort.js
+++ assets/js/de-resize_viewPort.js
@@ -31,8 +31,8 @@ document.addEventListener('mousemove', (e) => {
 
     viewport.style.transform = `translate(${A_currentX}px, ${A_currentY}px)`;
 
-   
-      
+
+
 });
 
 document.addEventListener('mouseup', () => {
@@ -45,32 +45,31 @@ viewport.style.cursor = 'grab';
 
 
 
-    document.addEventListener('mousemove', function(event) {
+document.addEventListener('mousemove', function (event) {
 
-        if (A_isDragging) {
+    if (A_isDragging) {
         // Get the cursor's X-coordinate
         const cursorX = event.clientX;
-      
+
         // Get the document's width
         const documentWidth = document.documentElement.clientWidth;
-      
+
         // Calculate the percentage position of the cursor
         const cursorPercentage = (cursorX / documentWidth) * 100;
-      
+
         // Check if the cursor is between 0% and 40% of the left side
         if (cursorPercentage >= 0 && cursorPercentage <= 40) {
-          // Cursor is within the desired range
-          console.log('Cursor is between 0% and 40% of the left side.');
-          document.getElementById('main').style.float = "right";
+            // Cursor is within the desired range
+            console.log('Cursor is between 0% and 40% of the left side.');
+            document.getElementById('main').style.float = "right";
 
 
         } else {
-          // Cursor is outside the desired range
-          console.log('Cursor is not in the specified range.');
-          document.getElementById('main').style.float = "left";
+            // Cursor is outside the desired range
+            console.log('Cursor is not in the specified range.');
+            document.getElementById('main').style.float = "left";
         }
 
     }
-      });
+});
 
-    
